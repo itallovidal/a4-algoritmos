cb69884386c285c54cc5c5fbd769c914a64455f1
Itallo Vidal <itallovidalll@gmail.com>
2025-04-15 18:24:33 -0300

    initialize files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <string.h>
+
+#define SELL_FILE_NAME "sell.txt"
+#define CLIENT_FILE_NAME "client.txt"
+
+FILE *initializeFile(const char fileName[20]) {
+  char filePath[100] = "data/";
+  strcat(filePath, fileName);
+
+  FILE *fileData = fopen(filePath, "r");
+
+  if (fileData != NULL) {
+    printf("\nArquivo carregado com sucesso!\n");
+    return fileData;
+  };
+
+  printf("\nArquivo não existe. Gerando...\n");
+  fileData = fopen(filePath, "w");
+  return fileData;
+}
 
 int main() {
   int isRunning = 1;
+
+  printf("\nInicializando arquivos.\n");
+  FILE *sellFile = initializeFile(SELL_FILE_NAME);
+  FILE *clientFile = initializeFile(CLIENT_FILE_NAME);
+
   while (isRunning) {
     printf("\nBem vindo ao seu sistema de PetShop!");
     printf("\nO que deseja fazer?");
@@ -12,7 +37,8 @@ int main() {
     printf("\n3 - Relatório de venda");
     printf("\n4 - Sair");
     printf("\n");
-    scanf("%d \n", &option);
+    scanf("%d", &option);
+    printf("\n");
 
     switch (option) {
     case 1:
@@ -29,5 +55,7 @@ int main() {
     }
   }
 
+  fclose(sellFile);
+  fclose(clientFile);
   return 0;
 }
