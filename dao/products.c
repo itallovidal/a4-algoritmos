cc8f08306776d3c71664fe9f08445813adbccc63
itallovidal <itallovidalll@gmail.com>
2025-05-06 15:10:33 -0300

    add get all products function

--- dao/products.c
+++ dao/products.c
@@ -1,4 +1,6 @@
-#include "products.h"
+#include "../include/products.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 struct ProductList getAllproducts() {
   FILE *file = fopen(PRODUCT_LIST_PATH, "r");
@@ -27,22 +29,22 @@ struct ProductList getAllproducts() {
   return list;
 }
 
-struct Product getProductByID(int id) {
-  struct ProductList productList = getAllproducts();
-  for (int i = 0; i < productList.count; i++) {
-    if (productList.product[i].id == id) {
-      return productList.product[i];
-      break;
-    }
-  }
-}
+// struct Product getProductByID(int id) {
+//   struct ProductList productList = getAllproducts();
+//   for (int i = 0; i < productList.count; i++) {
+//     if (productList.product[i].id == id) {
+//       return productList.product[i];
+//       break;
+//     }
+//   }
+// }
 
-int verifyProductID(struct ProductList *productList, int id) {
-  for (int i = 0; i < productList->count; i++) {
-    if (id == productList->product[i].id) {
-      return 1;
-    }
-  }
+// int verifyProductID(struct ProductList *productList, int id) {
+//   for (int i = 0; i < productList->count; i++) {
+//     if (id == productList->product[i].id) {
+//       return 1;
+//     }
+//   }
 
-  return 0;
-}
+//   return 0;
+// }
--- include/products.h
+++ include/products.h
@@ -1,24 +1,25 @@
-// #ifndef PRODUCTS
-// #define PRODUCTS
+#ifndef PRODUCTS
+#define PRODUCTS
 
-// #include <stdio.h>
-// #include <stdlib.h>
 // #include <string.h>
 // #include <time.h>
+#define PRODUCT_LIST_PATH "data/products.txt"
 
-// struct ProductList;
+struct ProductList {
+  struct Product * product;
+  int count;
+  int capacity;
+};
 
-// struct Product {
-//   int id;
-//   char name[50];
-//   char brand[50];
-//   float price;
-// };
+struct Product {
+  int id;
+  char name[50];
+  char brand[50];
+  float price;
+};
 
 
-// #define PRODUCT_LIST_PATH "data/products.txt"
-
-// struct ProductList getAllproducts();
+struct ProductList getAllproducts();
 // struct Product getProductByID(int id);
 // int verifyProductID(struct ProductList *productList, int id);
-// #endif
+#endif
